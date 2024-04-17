1916080fc74934f4480c9e6e9acfbc3d316e0e1d
shin-SeungHun <sng7387@gmail.com>
2024-04-17 18:31:03 +0900

    feat: dataBinding 적용한 로직 구현

--- app/src/main/java/com/ssh/java_study_mvvm_databinding_livadata/MainActivity.java
+++ app/src/main/java/com/ssh/java_study_mvvm_databinding_livadata/MainActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+
+import com.ssh.java_study_mvvm_databinding_livadata.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    private int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-}
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.minusBtn.setOnClickListener(v -> {
+            score--;
+            binding.scoreText.setText(String.valueOf(score));
+        });
+
+        binding.plusBtn.setOnClickListener(v -> {
+            score++;
+            binding.scoreText.setText(String.valueOf(score));
+        });
+
+    } // onCreate
+} // MainActivity
